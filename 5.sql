cb3cee45635c178fa299629f2fd78972fc5c8238
Ryan B <admirallasers@gmail.com>
2016-08-29 18:11:34 -0500

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT BillingCountry FROM Invoice
 
-Provide a query showing a unique list of billing countries from the Invoice table
+/* Provide a query showing a unique list of billing countries from the Invoice table
