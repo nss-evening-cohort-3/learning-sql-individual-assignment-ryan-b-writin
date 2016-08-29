8b3cd5417af8ca101b508d6f6cc835264cdbb821
Ryan B <admirallasers@gmail.com>
2016-08-29 18:10:40 -0500

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,3 +1,3 @@
 SELECT CustomerId,FirstName,LastName FROM Customer WHERE Country != "USA"
 
-//Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+/* Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
