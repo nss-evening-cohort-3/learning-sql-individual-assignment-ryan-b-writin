aa409e6aaba4c443a1cdb8b6ebff4ec5d4631722
Ryan B <admirallasers@gmail.com>
2016-08-29 18:38:58 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -2,4 +2,5 @@ SELECT Customer.FirstName ||" "|| Customer.LastName AS 'Customer Full Name',Coun
 JOIN Customer 
 ON Customer.CustomerId == Invoice.CustomerID WHERE Customer.Country IS "Brazil"
 
-/* Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country. */
+/* Provide a query showing the Invoices of customers who are from Brazil. 
+The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country. */
