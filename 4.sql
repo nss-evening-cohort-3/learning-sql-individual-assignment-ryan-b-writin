3ef824a143d213e6ccb7452a1ce0a06bc40b1f71
Ryan B <admirallasers@gmail.com>
2016-08-29 18:11:20 -0500

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,3 +1,3 @@
 SELECT EmployeeId,FirstName,LastName FROM Employee WHERE Title == "Sales Support Agent"
 
-Provide a query showing only the Employees who are Sales Agents.
+/* Provide a query showing only the Employees who are Sales Agents.
