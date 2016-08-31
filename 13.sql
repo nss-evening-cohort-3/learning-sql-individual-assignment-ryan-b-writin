028ed456b3227564948b8753ab3a6bedfd9f05c5
Ryan B <admirallasers@gmail.com>
2016-08-31 17:52:56 -0500

    Create 13.sql

---
+++ 13.sql
@@ -0,0 +1,7 @@
+/* Provide a query that includes the purchased track name AND artist name with each invoice line item. */
+
+SELECT InvoiceLineId, Track.Name AS 'Track Name', Artist.Name AS 'Artist Name' FROM InvoiceLine 
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Artist.ArtistId = Album.ArtistId
+ORDER BY InvoiceLineId
