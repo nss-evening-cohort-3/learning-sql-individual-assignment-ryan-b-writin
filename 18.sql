b1921bb40deecbd8eac7b079cb9fa98867eac8f2
Ryan B <admirallasers@gmail.com>
2016-08-31 18:51:19 -0500

    Create 18.sql

---
+++ 18.sql
@@ -0,0 +1,6 @@
+/* Provide a query that shows total sales made by each sales agent. */
+
+SELECT Employee.LastName ||', '|| Employee.FirstName AS 'EmployeeName', ROUND(SUM(Total),2) AS 'TotalPerRep' FROM Invoice
+JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+GROUP BY EmployeeName 
