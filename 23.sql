3060ff1332b3ee577ec074ba594bc123bb9a5691
Ryan B <admirallasers@gmail.com>
2016-08-31 21:21:26 -0500

    Update 23.sql

--- 23.sql
+++ 23.sql
@@ -1,9 +1,7 @@
 /* Provide a query that shows the total sales per country. Which country's customers spent the most? */
 
-SELECT BillingCountry, ROUND(SUM(Total), 2) AS NationalSales, TopBilledCountry FROM 
-(SELECT NationalSales.BillingCountry AS TopBilledCountry, MAX(NationalSales.TotalPerCountry) AS TopSpendingCountry FROM 
-(SELECT BillingCountry, SUM(Total) AS TotalPerCountry FROM Invoice GROUP BY BillingCountry) 
-AS NationalSales) AS Top
-JOIN Invoice
-GROUP BY BillingCountry;
-
+SELECT BillingCountry, ROUND(SUM(Total), 2) AS NationalSales, TopSpendingCountry FROM 
+(SELECT NationalSales.BillingCountry AS TopSpendingCountry, MAX(NationalSales.TotalPerCountry) AS TopSpent FROM 
+(SELECT BillingCountry, SUM(Total) AS TotalPerCountry FROM InvoiceGROUP BY BillingCountry) AS NationalSales) 
+AS Top 
+JOIN Invoice GROUP BY BillingCountry
