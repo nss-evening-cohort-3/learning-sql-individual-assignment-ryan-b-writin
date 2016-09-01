2546da6beeb48a641c72561139407fba38dd1189
Ryan B <admirallasers@gmail.com>
2016-08-31 20:15:55 -0500

    Create 19.sql

---
+++ 19.sql
@@ -0,0 +1,7 @@
+/* Which sales agent made the most in sales in 2009? */
+
+SELECT Sales2009.EmployeeName, MAX(Sales2009.TotalSales) FROM (SELECT Employee.LastName ||', '|| Employee.FirstName AS 'EmployeeName', Employee.EmployeeId, SUM(Total) AS 'TotalSales' From Invoice
+JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+WHERE InvoiceDate LIKE '2009%' 
+GROUP BY Employee.EmployeeId) AS 'Sales2009'
