6ec5381fdace5f980fbb3f9137dbf9147406cb90
Ryan B <admirallasers@gmail.com>
2016-09-03 08:57:01 -0500

    Update 23.sql

--- 23.sql
+++ 23.sql
@@ -2,6 +2,6 @@
 
 SELECT BillingCountry, ROUND(SUM(Total), 2) AS NationalSales, TopSpendingCountry FROM 
 (SELECT NationalSales.BillingCountry AS TopSpendingCountry, MAX(NationalSales.TotalPerCountry) AS TopSpent FROM 
-(SELECT BillingCountry, SUM(Total) AS TotalPerCountry FROM InvoiceGROUP BY BillingCountry) AS NationalSales) 
+(SELECT BillingCountry, SUM(Total) AS TotalPerCountry FROM Invoice GROUP BY BillingCountry) AS NationalSales) 
 AS Top 
 JOIN Invoice GROUP BY BillingCountry;
