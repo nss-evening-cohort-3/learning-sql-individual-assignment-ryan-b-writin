eb69ccca9cb7c535543040ef5ec32be9bbb8d73a
Ryan B <admirallasers@gmail.com>
2016-09-03 09:52:47 -0500

    Update 26.sql

--- 26.sql
+++ 26.sql
@@ -1,9 +1,9 @@
 /* Provide a query that shows the top 3 best selling artists. */
 
-SELECT Artist.Name, SUM(Quantity) * InvoiceLine.UnitPrice AS TotalSold FROM InvoiceLine
+SELECT Artist.Name, SUM(Quantity) * InvoiceLine.UnitPrice AS TotalSales FROM InvoiceLine
 JOIN Track ON InvoiceLine.TrackId = Track.TrackId
 JOIN Album ON Track.AlbumId = Album.AlbumId
 JOIN Artist ON Album.ArtistId = Artist.ArtistId
-GROUP BY InvoiceLine.TrackId
+GROUP BY Artist.Name
 ORDER BY TotalSold DESC
 LIMIT 3
