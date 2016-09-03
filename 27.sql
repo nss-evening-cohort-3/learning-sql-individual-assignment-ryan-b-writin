b6f76088a91202a430cd729d8dfed4352cac934f
Ryan B <admirallasers@gmail.com>
2016-09-03 10:00:48 -0500

    Create 27.sql

---
+++ 27.sql
@@ -0,0 +1,7 @@
+/* Provide a query that shows the most purchased Media Type. */
+
+SELECT MediaType, MAX(NumberSold) AS TotalSold FROM 
+(SELECT MediaType.Name AS MediaType, Track.MediaTypeId, InvoiceLine.TrackId, SUM(Quantity) AS NumberSold FROM InvoiceLine
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+JOIN MediaType ON MediaType.MediaTypeId = Track.MediaTypeId
+GROUP BY Track.MediaTypeId)
