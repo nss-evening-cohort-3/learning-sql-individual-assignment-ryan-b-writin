0d4f29d0aa26dc0121995706445a497005b63d7a
Ryan B <admirallasers@gmail.com>
2016-09-03 10:01:02 -0500

    Update 27.sql

--- 27.sql
+++ 27.sql
@@ -4,4 +4,4 @@ SELECT MediaType, MAX(NumberSold) AS TotalSold FROM
 (SELECT MediaType.Name AS MediaType, Track.MediaTypeId, InvoiceLine.TrackId, SUM(Quantity) AS NumberSold FROM InvoiceLine
 JOIN Track ON InvoiceLine.TrackId = Track.TrackId
 JOIN MediaType ON MediaType.MediaTypeId = Track.MediaTypeId
-GROUP BY Track.MediaTypeId)
+GROUP BY Track.MediaTypeId);
