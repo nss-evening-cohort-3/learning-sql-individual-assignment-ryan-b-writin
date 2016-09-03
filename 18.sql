36a40b71e4174cca74473fe28fe73941fc59e3bb
Ryan B <admirallasers@gmail.com>
2016-09-03 10:22:28 -0500

    Update 18.sql

--- 18.sql
+++ 18.sql
@@ -1,8 +1,7 @@
 /* Provide a query that shows total sales made by each sales agent. */
 
 SELECT Employee.LastName ||', '|| Employee.FirstName AS 'EmployeeName', 
-ROUND((SUM(Total) * InvoiceLine.UnitPrice),2) AS 'TotalPerRep' FROM Invoice
+ROUND(SUM(Total),2) AS 'TotalPerRep' FROM Invoice
 JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
 JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
-JOIN InvoiceLine ON Invoice.InvoiceId = InvoiceLine.InvoiceId
 GROUP BY EmployeeName 
