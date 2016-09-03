53ef8902d5709f7ae7724c66935f99dde186fdb4
Ryan B <admirallasers@gmail.com>
2016-09-03 09:50:42 -0500

    Update 25.sql

--- 25.sql
+++ 25.sql
@@ -1,8 +1,7 @@
 /* Provide a query that shows the top 5 most purchased tracks over all. */
 
-SELECT Track.Name AS TrackName,ROUND(SUM(Total),2) AS TotalSales FROM InvoiceLine 
-JOIN Invoice ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+SELECT Track.Name, SUM(Quantity) * InvoiceLine.UnitPrice AS TotalSold FROM InvoiceLine
 JOIN Track ON InvoiceLine.TrackId = Track.TrackId
-GROUP BY TrackName
-ORDER BY TotalSales DESC
+GROUP BY InvoiceLine.TrackId
+ORDER BY TotalSold DESC
 LIMIT 5
