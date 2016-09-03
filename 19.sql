a8471b586ac54f2afe5176e1622bb7c87743f8b8
Ryan B <admirallasers@gmail.com>
2016-09-03 10:21:06 -0500

    Update 19.sql

--- 19.sql
+++ 19.sql
@@ -2,9 +2,8 @@
 
 SELECT Sales2009.EmployeeName, MAX(Sales2009.TotalSales) FROM 
 (SELECT Employee.LastName ||', '|| Employee.FirstName AS 'EmployeeName', 
-Employee.EmployeeId, ROUND(SUM(Total) * InvoiceLine.UnitPrice, 2) AS 'TotalSales' From Invoice
+Employee.EmployeeId, ROUND(SUM(Total), 2) AS 'TotalSales' From Invoice
 JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
 JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
-JOIN InvoiceLine ON Invoice.InvoiceId = InvoiceLine.InvoiceId
 WHERE InvoiceDate LIKE '2009%' 
 GROUP BY Employee.EmployeeId) AS 'Sales2009';
