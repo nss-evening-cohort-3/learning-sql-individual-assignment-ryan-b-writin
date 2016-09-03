cffc64452b8da2a8ab3a72d7fc8f4bfdf0218f4d
Ryan B <admirallasers@gmail.com>
2016-09-03 10:03:26 -0500

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,1 +1,3 @@
-Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+/* Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37. */
+
+SELECT COUNT(*) FROM InvoiceLine WHERE InvoiceId = '37';
